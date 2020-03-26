5e0146899219dd3e246e714601f23abab472a5e3
Harry Clegg <mn14hrc@leeds.ac.uk>
2020-03-26 23:01:18 +0000

    Add ability to overwrite output folder

--- mpfree/UI/WindowController.swift
+++ mpfree/UI/WindowController.swift
@@ -9,12 +9,11 @@
 import Cocoa
 
 class WindowController: NSWindowController {
-        
+    
     var appDelegate: AppDelegate!
     var parser: ITParse!
     
-    @IBAction func doExport(_ sender: NSToolbarItem) {
-        
+    @IBAction func doExportAdd(_ sender: NSToolbarItem) {
         let panel = NSOpenPanel()
         panel.canChooseFiles = false
         panel.canChooseDirectories = true
@@ -29,11 +28,57 @@ class WindowController: NSWindowController {
             parser.exportPlaylists(exportURL: selectedURL)
         }
     }
+    
+    @IBAction func doExportOverwrite(_ sender: NSToolbarItem) {
         
+        let panel = NSOpenPanel()
+        panel.canChooseFiles = false
+        panel.canChooseDirectories = true
+        panel.allowsMultipleSelection = false
+        
+        panel.message = "Select a location to export the playlists to."
+        
+        let response = panel.runModal()
+        
+        if response == NSApplication.ModalResponse.OK {
+            guard let selectedURL = panel.url else { return }
+            
+            print(selectedURL.path)
+            
+            if FileManager.default.fileExists(atPath: selectedURL.path) {
+                
+                print("file exists")
+                
+                
+                // Remove the folder first.
+                do {
+                    try FileManager.default.removeItem(atPath: selectedURL.path)
+                } catch {
+                    print(error.localizedDescription);
+                }
+                
+                if FileManager.default.fileExists(atPath: selectedURL.path) {
+                    print("Failed to remove folder.")
+                }
+                
+            }
+            
+            
+            // Make a new folder in the same place.
+            do {
+                try FileManager.default.createDirectory(atPath: selectedURL.path, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print(error.localizedDescription);
+            }
+            
+            parser.exportPlaylists(exportURL: selectedURL)
+        }
+    }
+    
     
     override func windowDidLoad() {
         super.windowDidLoad()
-    
+        
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
         
         
@@ -41,5 +86,5 @@ class WindowController: NSWindowController {
         parser = appDelegate.parser
         
     }
-
+    
 }
